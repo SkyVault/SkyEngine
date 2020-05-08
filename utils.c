bbd625f58b439992305b081f42d24b2eff85a567
Dustin Neumann <dustinneumann42@gmail.com>
2020-05-08 12:24:01 -0700

    Created a basic temporary allocator

--- utils.c
+++ utils.c
@@ -1,39 +1,56 @@
 #include "utils.h"
 
-int getline(char **lineptr, size_t *n, FILE *stream)
-{
+int getline(char **lineptr, size_t *n, FILE *stream) {
     static char line[256];
     char *ptr;
     unsigned int len;
 
-    if (lineptr == NULL || n == NULL)
-    {
+    if (lineptr == NULL || n == NULL) {
         return -1;
     }
 
-    if (ferror(stream))
-        return -1;
+    if (ferror(stream)) return -1;
 
-    if (feof(stream))
-        return -1;
+    if (feof(stream)) return -1;
 
     fgets(line, 256, stream);
 
     ptr = strchr(line, '\n');
-    if (ptr)
-        *ptr = '\0';
+    if (ptr) *ptr = '\0';
 
     len = strlen(line);
 
-    if ((len + 1) < 256)
-    {
+    if ((len + 1) < 256) {
         ptr = realloc(*lineptr, 256);
-        if (ptr == NULL)
-            return (-1);
+        if (ptr == NULL) return (-1);
         *lineptr = ptr;
         *n = 256;
     }
 
     strcpy(*lineptr, line);
     return (len);
+}
+
+void tmem_reset() { TempMem.it = 0; }
+
+void *talloc(size_t n) {
+    uint8_t *start = TempMem.data + TempMem.it;
+    TempMem.it += n;
+    if (TempMem.it >= TMEM_SIZE) {
+        printf(
+            "ERROR:: Temporary allocator ran out of memory. Returning "
+            "malloc.\n");
+        return malloc(n);
+    }
+    return start;
+}
+
+char *tstrf(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    size_t len = snprintf(NULL, 0, fmt, args);
+    tstr result = talloc(len);
+    vsprintf(result, fmt, args);
+    va_end(args);
+    return result;
 }
--- utils.h
+++ utils.h
@@ -1,6 +1,8 @@
 #ifndef BENIS_UTILS_H
 #define BENIS_UTILS_H
 
+#include <stdarg.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,5 +11,22 @@
 #define TO_ENUM(x) x,
 
 int getline(char **lineptr, size_t *n, FILE *stream);
+
+#define TMEM_SIZE (0x800000)
+
+static struct {
+    uint8_t data[TMEM_SIZE];
+    size_t it;
+} TempMem = {
+    .data = {0},
+    .it = 0,
+};
+
+typedef char *tstr;
+
+void tmem_reset();
+void *talloc(size_t n);
+
+tstr tstrf(const char *fmt, ...);
 
 #endif  // BENIS_UTILS_H
