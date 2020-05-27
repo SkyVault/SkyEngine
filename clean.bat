ef10b3481e59c6bc47bd86dc39cb7e67443c477a
Dustin Neumann <dustinneumann42@gmail.com>
2020-05-27 11:08:47 -0700

    Greatly improved mouse placing using raycasting

---
+++ clean.bat
@@ -0,0 +1,3 @@
+CALL "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars32.bat"
+
+"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe" Benis3D.sln -target:Clean
