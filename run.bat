df0eaf35b5dd60edcb22fe1f3bcf96facb3d6aa7
Dustin Neumann <dustinneumann42@gmail.com>
2020-05-30 12:46:48 -0700

    We are now exlusively targeting x64 machines

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
-CALL "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars32.bat"
+CALL "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat"
 
-"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe" Benis3D.sln /p:useenv=true
-
+"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe" Benis3D.sln /p:useenv=true /p:Platform=x64 /t:Build
